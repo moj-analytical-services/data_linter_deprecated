c561d3ab13ae6c977108ee610ed8970a9486bf10
Karik Isichei <karik.isichei@gmail.com>
2019-06-20 11:23:40 +0100

    Made package a bit neater in creating linter object

--- data_linter/lint.py
+++ data_linter/lint.py
@@ -1,19 +1,68 @@
 import pandas as pd
 import numpy as np
 
-from etl_manager.meta import read_table_json
+from etl_manager.meta import read_table_json, TableMeta
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class DataframeNotSet(Error):
+    """
+    Exception raised if df property is None
+    """
+    def __init__(self, msg=None):
+        if msg is None:
+            # Set some default useful error message
+            msg = "Dataframe has not been set. Use read_df_from_* methods or set df property to a dataframe."
+        super(DataframeNotSet, self).__init__(msg)
+
+class TableMetaNotSet(Error):
+    """
+    Exception raised if table_meta property is None
+    """
+    def __init__(self, msg=None):
+        if msg is None:
+            # Set some default useful error message
+            msg = "table_meta has not been set. Use read_table_meta method or set table_meta property to a TableMeta objects."
+        super(TableMetaNotSet, self).__init__(msg)
 
 class Linter:
-    def __init__(self, datapath, metapath):
-        self.datapath = datapath,
-        self.metapath = metapath,
-        self.df = pd.read_csv(datapath, low_memory=False, dtype=object)
-        self.table_meta = read_table_json(metapath)
+    def __init__(self, df=None, table_meta=None):
+        """
+        Takes a pandas dataframe and a table meta data object and checks
+        the values in the dataframe against the meta data.
+        """
+        self.df = df
+        self.table_meta = table_meta
 
+    @property
+    def df(self):
+        return self._df
+
+    @df.setter
+    def df(self, df):
+        if df is not None and not isinstance(df, pd.DataFrame):
+            raise TypeError("df must be a pandas dataframe object")
+        else:
+            self._df = df
+    
+    @property
+    def table_meta(self):
+        return self._table_meta
+    
+    @table_meta.setter
+    def table_meta(self, table_meta):
+        if table_meta is not None and not isinstance(table_meta, TableMeta):
+            raise TypeError("table_meta must be a TableMeta object")
+        else:
+            self._table_meta = table_meta
+    
     def test_columns(self):
         """
         Returns test results of each column
         """
+        self._check_df_and_meta_are_set()
         resp = {}
 
         for c in self.table_meta.column_names:
@@ -33,6 +82,7 @@ class Linter:
 
         col_name: (String) name of the column to test.
         """
+        self._check_df_and_meta_are_set()
         m = self.meta_col(col_name)
         if 'enum' not in m:
             return None
@@ -47,6 +97,7 @@ class Linter:
 
         col_name: (String) name of the column to test.
         """
+        self._check_df_and_meta_are_set()
         m = self.meta_col(col_name)
         if 'regex' not in m:
             return None
@@ -63,6 +114,7 @@ class Linter:
 
         col_name: (String) name of the column to test.
         """
+        self._check_df_and_meta_are_set()
         m = self.meta_col(col_name)
         if 'nullable' not in m:
             return None
@@ -80,12 +132,42 @@ class Linter:
 
         col_name: (String) name of the column to test.
         """
+        self._check_df_and_meta_are_set(check_meta=False)
         arr = self.df[col_name].isnull()
         if empty_str_as_null:
             arr = (arr) | (self.df[col_name] == '')
         return arr
 
     def meta_col(self, col_name):
+        self._check_df_and_meta_are_set(check_df=False)
         if col_name not in self.table_meta.column_names:
             raise ValueError(f"col_name: {col_name} not found in meta data columns.")
         return [c for c in self.table_meta.columns if c['name'] == col_name][0]
+
+    def _check_df_and_meta_are_set(self, check_df=True, check_meta=True):
+        if check_df and self.df is None:
+            raise DataframeNotSet()
+        if check_meta and self.table_meta is None:
+            raise TableMetaNotSet()
+
+    def read_table_meta(self, path):
+        """
+        Reads in the table meta data object
+        """
+        self.table_meta = read_table_json(path)
+
+    def read_df_from_csv(self, path, *args, **kwargs):
+        """
+        Reads in pandas dataframe from the provided path of a csv file. Args and kwargs are
+        passed to pd.read_csv function. Note low_memory is set to False and dtype is set to
+        object.
+        """
+        self.df = pd.read_csv(path, low_memory=False, dtype=object, *args, **kwargs)
+
+    def read_df_from_jsonl(self, path, *args, **kwargs):
+        """
+        Reads in pandas dataframe from the provided path of a jsonl file. Args and kwargs are 
+        passed to pd.read_csv function. Note low_memory is set to False and dtype is set to
+        object.
+        """
+        self.df = pd.read_json(path, dtype=object, lines=True, numpy=True, *args, **kwargs)
--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,7 +1,7 @@
 import unittest
 
 import os
-from data_linter.lint import Linter
+from data_linter.lint import Linter, DataframeNotSet, TableMetaNotSet
 
 class BasicDataTest(unittest.TestCase):
 
@@ -10,7 +10,10 @@ class BasicDataTest(unittest.TestCase):
         data_path = os.path.join(current_dir, 'data/pass_data.csv')
         meta_path = os.path.join(current_dir, 'data/meta.json')
 
-        dl = Linter(data_path, meta_path)
+        dl = Linter()
+        dl.read_df_from_csv(data_path)
+        dl.read_table_meta(meta_path)
+
         resp = dl.test_columns()
         expected = {
             "col1": {
@@ -38,7 +41,10 @@ class BasicDataTest(unittest.TestCase):
         data_path = os.path.join(current_dir, 'data/fail_data.csv')
         meta_path = os.path.join(current_dir, 'data/meta.json')
 
-        dl = Linter(data_path, meta_path)
+        dl = Linter()
+        dl.read_df_from_csv(data_path)
+        dl.read_table_meta(meta_path)
+
         resp = dl.test_columns()
         expected = {
             "col1": {
@@ -60,3 +66,8 @@ class BasicDataTest(unittest.TestCase):
         self.assertDictEqual(expected['col1'], resp['col1'])
         self.assertDictEqual(expected['col2'], resp['col2'])
         self.assertDictEqual(expected['col3'], resp['col3'])
+
+    def test_errors(self):
+        dl = Linter()
+        self.assertRaises(DataframeNotSet, dl.get_nullable_values, "col1")
+        self.assertRaises(TableMetaNotSet, dl.meta_col, "col1")
