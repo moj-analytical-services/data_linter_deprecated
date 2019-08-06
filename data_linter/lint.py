2c9fa25f21babd047eee2c6b48bc1b8a3308fa93
GeorgeKelly <georgekelly@L0311-Pro.local>
2019-08-06 11:13:57 +0100

    added function to check enums in metadata files

--- data_linter/lint.py
+++ data_linter/lint.py
@@ -58,3 +58,31 @@ class Linter:
         # Apply to log not return
         # log["col1"]["check_column_name_and_order"] = BLAH
         return self.df.expect_table_columns_to_match_ordered_list(self.meta_colnames, result_format="COMPLETE", catch_exceptions=True)
+
+    def check_enums(self):
+        """
+        Test to if values in column are all in 
+        enums as specified in metadata
+        """
+         
+        print("Running enum test")
+
+        test_name = "check_enums"
+        
+        for col in self.meta_cols:
+
+            try:
+                enum_list = col["enum"]
+            except KeyError:
+                
+                enum_result = {}
+                continue
+            
+            enum_result = \
+            self.df.expect_column_values_to_be_in_set(col["name"],
+                                                      enum_list,
+                                                      result_format="COMPLETE", 
+                                                      include_config=False, 
+                                                      catch_exceptions=True)
+            
+            self.log[col["name"]][test_name] = enum_result
