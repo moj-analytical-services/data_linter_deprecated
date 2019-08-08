810d0f32c5dc226afbd7bf44ed097f7bfeb1b422
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-08-08 12:02:38 +0100

    tests should now pass

--- tests/test_log_class.py
+++ tests/test_log_class.py
@@ -41,70 +41,51 @@ class TestLogMethods(unittest.TestCase):
         result = l.vlog["mychar"]["check_enums"].as_dict()
         self.assertDictEqual(result, expected_result)
 
-    # @parameterized.expand(
-    #     [
-    #         (
-    #             "test_csv_data_valid",
-    #             "meta/test_meta_cols_valid.json",
-    #             "expected_results/test_check_column_exists_and_order/data_valid.json",
-    #         ),
-    #         (
-    #             "test_csv_data_missing_col",
-    #             "meta/test_meta_cols_valid.json",
-    #             "expected_results/test_check_column_exists_and_order/missing_col.json",
-    #         ),
-    #         (
-    #             "test_csv_data_valid_wrong_order",
-    #             "meta/test_meta_cols_valid.json",
-    #             "expected_results/test_check_column_exists_and_order/wrong_order.json",
-    #         ),
-    #     ]
-    # )
-    # def test_check_column_exists_and_order(self, d, m, r):
-    #     df = get_test_csv(d)
-    #     meta = read_json(m)
-    #     result = read_json(r)
-    #     L = Linter(df, meta)
-    #     L.check_column_exists_and_order()
+    @parameterized.expand(
+        [
+            (
+                "test_csv_data_valid",
+                "meta/test_meta_cols_valid.json",
+                "expected_results/test_check_column_exists_and_order/data_valid.json",
+            ),
+            (
+                "test_csv_data_missing_col",
+                "meta/test_meta_cols_valid.json",
+                "expected_results/test_check_column_exists_and_order/missing_col.json",
+            ),
+            (
+                "test_csv_data_valid_wrong_order",
+                "meta/test_meta_cols_valid.json",
+                "expected_results/test_check_column_exists_and_order/wrong_order.json",
+            ),
+        ]
+    )
+    def test_check_column_exists_and_order(self, d, m, r):
+        df = get_test_csv(cwd, d)
+        meta = read_json(cwd, m)
+        expected_result = read_json(cwd, r)
+        l = Linter(df, meta)
+        l.check_column_exists_and_order()
+        result = l.vlog.as_dict()
+        self.assertDictEqual(result, expected_result)
 
-    #     self.assertDictEqual(L.log, result)
+    @parameterized.expand(
+        [
+            (
+                "test_csv_data_invalid_regex",
+                "meta/test_meta_cols_regex.json",
+                "expected_results/test_result_invalid_regex.json",
+            )
+        ]
+    )
+    def test_check_regex(self, d, m, r):
 
-    # @parameterized.expand(
-    #     [
-    #         (
-    #             "test_csv_data_invalid_regex",
-    #             "meta/test_meta_cols_regex.json",
-    #             "expected_results/test_result_invalid_regex.json",
-    #         )
-    #     ]
-    # )
-    # def test_check_regex(self, d, m, r):
+        df = get_test_csv(cwd,d)
+        meta = read_json(cwd,m)
+        expected_result = read_json(cwd,r)
 
-    #     df = get_test_csv(d)
-    #     meta = read_json(m)
-    #     result = read_json(r)[0]
+        l = Linter(df, meta)
+        l.check_pattern()
+        result = l.vlog.as_dict()
+        self.assertDictEqual(result, expected_result)
 
-    #     L = Linter(df, meta)
-    #     L.check_pattern()
-    #     self.assertDictEqual(L.log, result)
-
-    # def test_validate_meta_data(self):
-    #     meta = read_json("meta/test_meta_cols_valid.json")
-
-    #     #Data is irrelevant but cannot instantiate linter without it
-    #     df = get_test_csv("test_csv_data_valid")
-
-    #     # Test no error is raised
-    #     L = Linter(df, meta)
-
-    #     # Test invalid metadata raises an error
-    #     meta = read_json("meta/test_invalid_meta_cols_missing_name.json")
-
-    #     with self.assertRaises(ValidationError):
-    #         L = Linter(df, meta)
-
-    #     # Test invalid metadata raises an error
-    #     meta = read_json("meta/test_invalid_meta_columns_key_mispelt.json")
-
-    #     with self.assertRaises(ValidationError):
-    #         L = Linter(df, meta)
