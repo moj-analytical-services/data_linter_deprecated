b6885fe5122f6edca835b79d828888c3cad96475
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-08-21 11:16:45 +0100

    jinja tempaltes

--- data_linter/validation_log.py
+++ data_linter/validation_log.py
@@ -4,6 +4,9 @@ import pandas as pd
 import numpy as np
 import tabulate
 
+from jinja2 import Environment, PackageLoader
+jinja_env = Environment(loader=PackageLoader('data_linter', 'templates'))
+
 class ValidationLog:
     """
     Stores the log produced by the linter
@@ -62,14 +65,97 @@ class ValidationLog:
 
     def as_detailed_markdown(self):
         ## TODO:  Add here's a few sample rows of data:
-        result = []
-        for k, v in self._vlog.items():
-            result.extend(v.as_markdown())
-        return "".join(result)
+
+        md = """
+
+        """
+
+        ind_results = [v.as_markdown() for v in self._vlog.values()]
+
+
+        return "".join(ind_results)
 
 
     def _repr_markdown_(self):
             return self.as_summary_markdown()
+
+
+class ColumnLogEntries:
+    """
+    A collection of LogEntry objects, one for each function
+
+    ColumnLogEntries knowns column name
+    """
+
+    def __init__(self, col_name, linter):
+        self.col_name = col_name
+        self.linter = linter
+        self.entries = {}
+
+    def create_logentry_from_ge_result(self, validation_description, ge_output):
+        le = self[validation_description]
+        le.success = ge_output["success"]
+
+        for key, value in ge_output["result"].items():
+            le.set_result_key(key, value)
+
+        if "exception_info" in ge_output:
+            for key, value in ge_output["exception_info"].items():
+                le.set_exception_info_key(key, value)
+
+    def __getitem__(self, key):
+        if key not in self.entries:
+            le = LogEntry(self.col_name, key, self.linter)
+            self.entries[key] = le
+        else:
+            le = self.entries[key]
+        return le
+
+    # Serialisation/presentation functions
+    def as_dict(self):
+        return {k: v.as_dict() for k, v in self.entries.items()}
+
+    def as_table_rows(self):
+        return [v.as_table_row() for k, v in self.entries.items()]
+
+    def success(self):
+
+        success_array = [v.success for k, v in self.entries.items()]
+
+        if None in success_array:
+            raise Exception(
+                "Some tests have failed to return a result.  This indicates a bug.")
+
+        if len(success_array) == 0:
+            raise Exception(
+                f"No tests have yet been run for {self.col_name}.  You probably need to run linter.check_all()")
+
+        if False in success_array:
+            return False
+
+        if all(success_array):
+            return True
+
+    def as_markdown(self):
+
+        mds = [v.as_markdown() for v in self.entries.values()]
+        template = jinja_env.get_template('logentries_detailed.j2')
+        return template.render(jinja_data)
+
+        return f"""
+---
+
+### Results for column {self.col_name}
+
+{individual_results}
+"""
+
+    def __repr__(self):
+        repr = ""
+        for key, value in self.entries.items():
+            repr += value.__repr__() + "\n"
+
+        return repr
 
 
 class LogEntry:
@@ -133,7 +219,9 @@ class LogEntry:
     def get_failure_rows(self, n=5):
         if "unexpected_index_list" in self.result:
             indices =  self.result["unexpected_index_list"][:n]
-            return self.df_ge.loc[indices, :].to_dict(orient='records')
+            df = self.df_ge.loc[indices, :].copy()
+            df = df.reset_index()
+            return df
         else:
              return []
 
@@ -152,92 +240,30 @@ class LogEntry:
             "success": self.success
         }
 
-    def as_markdown(self):
-        md = f"{self._status_emoji()} The *{self.validation_description}* check was a {self._status_string().lower()}."
-        failure_rows = self.get_failure_rows(2)
-        if len(failure_rows) > 0:
-            md = md + "Here's a sample of some rows which failed:\n\n" + tabulate.tabulate(failure_rows, headers="keys", tablefmt="pipe")
-            unexpected_values = ", ".join(self.result["unexpected_list"][2:10])
-            if unexpected_values:
-                md = md + f"\n\n Some more values in the column which failed were {unexpected_values}"
-        return md
+    def as_markdown(self, table_rows = 2, unexpected_values = 8):
+
+        unexpected_list = self.result["unexpected_list"][table_rows:table_rows+unexpected_values]
+        unexpected_index = self.result["unexpected_index_list"][table_rows:table_rows+unexpected_values]
+
+        unexpected_data = [{"index": v[0], "value":v[1].__repr__()} for v in zip(unexpected_index, unexpected_list)]
+
+        jinja_data = {
+            "validation_description": self.validation_description,
+            "status_emoji": self._status_emoji(),
+            "status_string": self._status_string(),
+            "table_exists": len(self.result["unexpected_list"]) > 0,
+            "table": tabulate.tabulate(self.get_failure_rows(table_rows), headers="keys", tablefmt="pipe", showindex=False),
+            "unexpected_list_exists": len(self.result["unexpected_list"]) > table_rows,
+            "unexpected_data": unexpected_data
+
+        }
+
+        template = jinja_env.get_template('logentry_detailed.j2')
+        return template.render(jinja_data)
+
 
 
     def __repr__(self):
         return f"data_linter.validation_log.LogEntry: This log entry checks {self.validation_description} for column {self.col_name}.  Current status: {self._status_string()}"
 
 
-class ColumnLogEntries:
-    """
-    A collection of LogEntry objects, one for each function
-
-    ColumnLogEntries knowns column name
-    """
-
-    def __init__(self, col_name, linter):
-        self.col_name = col_name
-        self.linter = linter
-        self.entries = {}
-
-    def create_logentry_from_ge_result(self, validation_description, ge_output):
-        le = self[validation_description]
-        le.success = ge_output["success"]
-
-        for key, value in ge_output["result"].items():
-            le.set_result_key(key, value)
-
-        if "exception_info" in ge_output:
-            for key, value in ge_output["exception_info"].items():
-                le.set_exception_info_key(key, value)
-
-    def __getitem__(self, key):
-        if key not in self.entries:
-            le = LogEntry(self.col_name, key, self.linter)
-            self.entries[key] = le
-        else:
-            le = self.entries[key]
-        return le
-
-    # Serialisation/presentation functions
-    def as_dict(self):
-        return {k:v.as_dict() for k,v in self.entries.items()}
-
-    def as_table_rows(self):
-        return [v.as_table_row() for k, v in self.entries.items()]
-
-    def success(self):
-
-        success_array = [v.success for k, v in self.entries.items()]
-
-        if None in success_array:
-            raise Exception("Some tests have failed to return a result.  This indicates a bug.")
-
-        if len(success_array) == 0:
-            raise Exception(f"No tests have yet been run for {self.col_name}.  You probably need to run linter.check_all()")
-
-        if False in success_array:
-            return False
-
-        if all(success_array):
-            return True
-
-    def as_markdown(self):
-
-        mds = [v.as_markdown() for k, v in self.entries.items()]
-        individual_results = "\n\n".join(mds)
-
-        return f"""
----
-
-### Results for column {self.col_name}
-
-{individual_results}
-"""
-
-
-    def __repr__(self):
-        repr = ""
-        for key, value in self.entries.items():
-            repr += value.__repr__() +"\n"
-
-        return repr
